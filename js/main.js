e877f3f7b8decca7a1b100bb1a31516543d24db9
AnKoch73 <ankoch@ankoch.ru>
2022-07-19 12:26:23 +0300

    аааа

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 // var $btnTop = $(".btn_top")
-$(window).on("scroll", function(){
-	if ($(window).scrollTop() >= 400){
+$(window).on("scroll", function () {
+	if ($(window).scrollTop() >= 400) {
 		$(".btn_top").fadeIn();
-	}else{
+	} else {
 		$(".btn_top").fadeOut();
 	}
 });
-$(".btn_top").on("click", function(e){
-	$("html, body").animate({scrollTop:0});
+$(".btn_top").on("click", function (e) {
+	$("html, body").animate({ scrollTop: 0 });
+});
+$("#main").on("click", function (e) {
+	$(this).toggleClass("animate__animated animate__zoomIn");
 });
